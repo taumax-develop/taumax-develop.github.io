a6e91582ece350b59c7517743f6cec0eb73bde30
DESKTOP-3KOP9PP\flo_p <flo_pekoe_psyche@yahoo.co.jp>
2025-07-09 19:32:27 +0900

    astro-taumaxからtaumax-developに移設

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: 'never',
   site: 'https://taumax-develop.github.io',
   integrations: [mdx(), sitemap()],
 });
