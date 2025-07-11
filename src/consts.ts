c8e4c499740e7c56b3aee3dff9ef08eb2ef932fe
DESKTOP-3KOP9PP\flo_p <flo_pekoe_psyche@yahoo.co.jp>
2025-07-12 00:59:56 +0900

    astro-taumaxからtaumax-developに移設

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,9 @@
 
 export const SITE_TITLE = 'システム開発知識が集うサイト';
 export const SITE_DESCRIPTION = 'システム開発知識が集うサイト';
+export let BASE_URL = () => {
+  if (import.meta.env.PROD) {
+    return "https://taumax-develop.github.io"
+  }
+  return "http://localhost:4321"
+}
