ae3ef11362a5e59e818648b4c32ed7fe284d4cb9
DESKTOP-3KOP9PP\flo_p <flo_pekoe_psyche@yahoo.co.jp>
2025-07-12 22:05:53 +0900

    layoutファイル修正

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,9 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'システム開発知識が集うサイト';
-export const SITE_DESCRIPTION = 'システム開発知識が集うサイト';
+export const SITE_DESCRIPTION = 'システム開発に関する知識を集めたサイト';
+export const TWITTER_CARD_SUMMARY = 'summary'
+export const TWITTER_CARD_LARGE = 'summary_large_image'
 export const BASE_URL: string = function() {
   let baseUrl = "https://taumax-develop.github.io"
   if (import.meta.env.DEV) {
